9da8a7ebbb06c95473f460039850d3f934036673
Alba <albadvg@yahoo.es>
2023-03-16 15:41:35 +0100

    odd parade

--- js/script.js
+++ js/script.js
@@ -1,28 +1,78 @@
+//VARIABLES
+let homeButton = document.querySelector('#home-button');
+
 let navElements = document.querySelectorAll('.nav-element');
-let presentation = document.querySelector('#presentation');
 
-for (i=0; i<navElements.length; i++) {
-    navElements[i].addEventListener('mouseover' , hideSection);
-};
-
-function hideSection() {
-    presentation.classList.add('hidden');
-    paradeSection.classList.add('hidden');
-}
-
-//////////////////////////////////////////////////////////////
+let home = document.querySelector('#home');
 
 let paradeButton = document.querySelector('#parade-button');
 let paradeSection = document.querySelector('#odd-parade');
+let paradeSlider = document.querySelector('.slides-wrapper');
+let paradeSlides = document.querySelectorAll('.slide');
 
-paradeButton.addEventListener('click', showSection);
+let musicDiv = document.querySelector('#music');
+let audioBackground = document.querySelector('#audio-background');
+let audioElement = document.querySelector('#music audio');
 
-function showSection() {
-    paradeSection.style.visibility = 'visible';
-    if (paradeSection.classList.contains('hidden')){
-        paradeSection.classList.remove('hidden');
-    };
-    if (!presentation.classList.contains('hidden')){
-        presentation.classList.add('hidden');
-    };
+
+
+//EVENT LISTENERS
+homeButton.addEventListener('click', showHome)
+
+for (i=0; i<navElements.length; i++) {
+    navElements[i].addEventListener('mouseover' , hideHome);
+};
+
+paradeButton.addEventListener('click', showParade);
+
+audioElement.addEventListener('pause', pauseParade);
+audioElement.addEventListener('play', playParade);
+
+
+
+//FUNCTIONS
+//hides home page when hovering names in menu
+function hideHome() {
+    home.style.display = 'none';
+    paradeSection.style.display = 'none';
+};
+//shows home page when clicking home icon and hiddes audio section if already visible
+function showHome(){
+    home.style.display = 'block';
+    paradeSection.style.display = 'none';
+    if(musicDiv.style.display === 'flex') {
+        audioBackground.classList.add('hide-to-right'); 
+        audioElement.classList.add('hide-to-right');    
+    }
+}
+
+//shows parade section when clicking button 'Odd Parade" and moves audio section to full width
+function showParade() {
+    home.style.display = 'none';
+    paradeSection.style.display = 'block';
+    musicDiv.style.display = 'flex';
+    audioElement.play(); 
+    if(audioBackground.classList.contains('hide-to-right')) {
+        audioBackground.classList.remove('hide-to-right');
+        audioElement.classList.remove('hide-to-right');
+    } ;
+};
+
+// pauses parade animation when pausing the music
+function pauseParade(){
+    if (paradeSection.style.display === 'block'){
+        paradeSlider.style.animationPlayState = 'paused';
+        paradeSlides.forEach(slide => { 
+            slide.style.animationPlayState = 'paused';
+        });
+    }    
+};
+//replays parade animation when replaying music
+function playParade(){
+    if (paradeSection.style.display === 'block'){
+        paradeSlider.style.animationPlayState = 'running';
+        paradeSlides.forEach(slide => { 
+            slide.style.animationPlayState = 'running';
+        });
+    }    
 };
